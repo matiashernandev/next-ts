14926c699d92918e16ce598953d07f116c846263
nkwaaaa <nkwaaaa@gmail.com>
2023-03-14 11:49:05 -0300

    add lodash

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { MouseEventHandler } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { LazyImage } from "@/components/RandomFox";
+import { random } from "lodash";
 
 // generate simple unique id
 const generateId = (): string => {
@@ -13,14 +14,15 @@ const generateId = (): string => {
 };
 
 // random number from 1 to 122
-const random = () => Math.floor(Math.random() * 122) + 1;
+//const random = () => Math.floor(Math.random() * 122) + 1;
+const myRandom = () => random(1, 122);
 
 const Home: NextPage = () => {
 	const [images, setImages] = useState<Array<IImageItem>>([]);
 
 	const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
 		const id = generateId();
-		const url = `https://randomfox.ca/images/${random()}.jpg`;
+		const url = `https://randomfox.ca/images/${myRandom()}.jpg`;
 		setImages([...images, { id, url }]);
 	};
 
